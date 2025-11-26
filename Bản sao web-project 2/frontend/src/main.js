01ba19537be2ea3e003f7a8d96d106627b2f0343
NguyenMinhTri2908 <nguyenminhtritkqt@gmail.com>
2025-11-26 16:37:27 +0700

    Delete Bản sao web-project 2 directory

--- Bản sao web-project 2/frontend/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router'; // Import router đã cấu hình
-import 'fomantic-ui-css/semantic.min.css'; // Import CSS giao diện
-
-const app = createApp(App);
-app.use(router); // Sử dụng router
-app.mount('#app');
